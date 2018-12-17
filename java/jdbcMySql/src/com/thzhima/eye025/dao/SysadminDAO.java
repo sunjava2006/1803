70228379ac8a34248aa449767a89e1dccc361cb7
Wang Rui <jkwangrui@126.com>
2018-12-17 17:36:20 +0800

    2018-12-17

--- java/jdbcMySql/src/com/thzhima/eye025/dao/SysadminDAO.java
+++ java/jdbcMySql/src/com/thzhima/eye025/dao/SysadminDAO.java
@@ -1,34 +1,31 @@
 package com.thzhima.eye025.dao;
 
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.thzhima.eye025.bean.Sysadmin;
 
 public class SysadminDAO {
 
-	public Sysadmin login(String adminName, String password) throws SQLException  {
-		//Map<String, Object> map = null;
+	public static  Sysadmin login(String adminName, String password) throws Exception  {
+	
 		Sysadmin s = null;
-		Driver driver  = new org.mariadb.jdbc.Driver();
-		
 		Connection conn = null;
-		Statement stm = null;
+		PreparedStatement stm = null;
 		ResultSet rst = null;
-		String url = "jdbc:mariadb://localhost:3306/eye025";
-		String user = "eye025";
-		String pwd = "123456";
-		String sql = "select * from sys_admin where admin_name='" + adminName + "' and password=md5('" + password + "')";
+		
+		String sql = "select * from sys_admin where admin_name=? and password=md5(?)";
+		
+		System.out.println(sql);
 		try {
-			conn = DriverManager.getConnection(url, user, pwd);
-			stm = conn.createStatement();
-			rst = stm.executeQuery(sql);
+			conn = ConnectionUtil.getConnection();
+			stm = conn.prepareStatement(sql);   // 使用预编译语句对象。 改变以给语句中变量的赋值方式。
+			stm.setString(1, adminName);   // 先预编译语句，再给变量赋值。
+			stm.setString(2, password);
+			rst = stm.executeQuery();
 			
 			if(rst.next()) {
 				int id = rst.getInt("id");
@@ -36,17 +33,12 @@ public class SysadminDAO {
 				int superAdmin = rst.getInt("super_admin");
 				
 				s = new Sysadmin(id, name, adminName, password, superAdmin);
-				
-//				map = new HashMap<>();
-//				map.put("id", id);
-//				map.put("name", name);
-//				map.put("admin_name", adminName);
-//				map.put("password", password);
-//				map.put("super_admin", superAdmin);
+
 			}
 			
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
+			throw e;
 		} finally {
 			if(null != rst) {
 				rst.close();
@@ -63,21 +55,100 @@ public class SysadminDAO {
 	}
 	
 
-	
-	public int divide(int a , int b)throws Exception {
-		if(0 == b) {
-			throw new Exception("参数b,不能为0.");
-		}else {
-			return a/b;
+	public static int addUser(String name, String adminName, String password, boolean isSuperAdmin) throws SQLException {
+		int count = -1;
+		String sql = "insert into sys_admin(name, admin_name, password, super_admin) values (?,?,md5(?),?) ";
+			
+		
+		Connection conn = null;
+		PreparedStatement stm = null;
+		try {
+			conn = ConnectionUtil.getConnection();
+			conn.setAutoCommit(false);
+			stm = conn.prepareStatement(sql);
+			stm.setString(1, name);
+			stm.setString(2, adminName);
+			stm.setString(3, password);
+			stm.setInt(4, isSuperAdmin?1:0);
+			
+			count = stm.executeUpdate(); // 增、删、改操作，都是调用executeUpdate方法。返回int值，表示该语句影响的行数。
+			conn.commit();
+		} catch (ClassNotFoundException | SQLException e) {
+			if(null != conn) {
+				conn.rollback();
+			}
+			
+			e.printStackTrace();
+		} finally {
+			if(null != stm) {
+				stm.close();
+			}
+			if(null != conn) {
+				conn.setAutoCommit(true);
+				conn.close();
+			}
 		}
+		return count;
+	}
+	
+	public static int updateUserWithID(String name, String adminName, String password, boolean isSuperAdmin, int id) throws SQLException {
+		int count = -1;
+		String sql = "update sys_admin set name='"+name+"', admin_name='"+adminName
+				+"', password=md5('"+password+"'), super_admin="+(isSuperAdmin?1:0)+" where id=" + id;
+		Connection conn = null;
+		Statement stm = null;
+		try {
+			conn = ConnectionUtil.getConnection();
+			stm = conn.createStatement();
+			count = stm.executeUpdate(sql);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(null != stm) {
+				stm.close();
+			}
+			if(null != conn) {
+				conn.close();
+			}
+		}
+		
+		
+		return count;
+	}
+	
+	public static int deleteWithID(int id) throws SQLException {
+		int count = -1;
+		String sql = "delete from sys_admin where id="+id;
+		Connection conn = null;
+		Statement stm = null;
+		try {
+			conn = ConnectionUtil.getConnection();
+			stm = conn.createStatement();
+			count = stm.executeUpdate(sql);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(null != stm) {
+				stm.close();
+			}
+			if(null != conn) {
+				conn.close();
+			}
+		}
+		return count;
 	}
 	
 	
-	public static void main(String[] args) throws SQLException {
-		SysadminDAO dao = new SysadminDAO();
-		Sysadmin m = dao.login("ying","123456");
-		System.out.println(m);
+	
+	public static void main(String[] args) throws Exception {
+//		Sysadmin a = SysadminDAO.login("LiXin", "123456");  // 语句注入，侵入系统。语句注入的目的是，改变原来语句的意思。
+//		System.out.println(a);
+	   
+		int count = SysadminDAO.addUser("张亮44", "ZhangLiang", "123456", true);
+		System.out.println(count);
 		
+
+	
 				
 	}
 	
