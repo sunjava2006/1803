86aeaba3e577b2223879df8e0dceb3303f461662
Wang Rui <jkwangrui@126.com>
2018-12-21 14:38:25 +0800

    socket

--- java/eye025/src/com/thzhima/eye025/servlets/LoginServlet.java
+++ java/eye025/src/com/thzhima/eye025/servlets/LoginServlet.java
@@ -2,48 +2,65 @@ package com.thzhima.eye025.servlets;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Enumeration;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 
 import com.thzhima.eye025.bean.Sysadmin;
 import com.thzhima.eye025.service.UserService;
 
 
 public class LoginServlet implements Servlet{
-
+	private String contentType ;
+	
+	public LoginServlet() {
+		System.out.println("============构造中=============");
+	}
+	
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
+	public void init(ServletConfig cfg) throws ServletException {
+		System.out.println("============init中=============");
+		String charset = cfg.getInitParameter("charset");
+		this.contentType = "text/html;charset="+charset;
 		
 	}
-
-	@Override
-	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getServletInfo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void init(ServletConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType(this.contentType);
+		
 		String adminName = request.getParameter("adminName"); // 获取用户发送的请求参数
 		String password = request.getParameter("password");
+		//=================================================
+		System.out.println(request.getRemoteHost());
+		System.out.println(request.getServerName());
+		System.out.println(request.getCharacterEncoding());
+		System.out.println(request.getContentLength());
+		System.out.println(request.getLocalAddr());
+		
+		System.out.println(request.getProtocol());
+		
+		System.out.println(request.getRemotePort());
+		System.out.println(request.getLocalPort());
+		System.out.println(request.getServerPort());
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		
+		
+		Enumeration<String> heads = req.getHeaderNames();
+		while(heads.hasMoreElements()) {
+			String name = heads.nextElement();
+			String val = req.getHeader(name);
+			System.out.println(name + ":" + val);
+		}
+		
+		
+		//==================================================
 		boolean ok = true;
 		
 		Sysadmin user = null;
@@ -61,13 +78,37 @@ public class LoginServlet implements Servlet{
 		}
 		else {
 			if(user != null) {
-				response.getWriter().print(" login success. "); // 响应。输出给浏览器的内容。
+				
+				response.getWriter().print(" login success. "+ user.getName()); // 响应。输出给浏览器的内容。
 			}else {
 				response.getWriter().print(" user name or password error. "); // 响应。输出给浏览器的内容。
 			}
 		}
-		
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("============将要关闭=============");
 		
 	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		System.out.println("============getServletConfig中=============");
+		
+		
+		
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		System.out.println("============getServletInfo中=============");
+		return null;
+	}
+
+	
+
+
 
 }
